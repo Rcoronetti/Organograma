58bc369897912c3344b04c9567009789a1ec2ff4
Rafael Coronetti <rafael.coronetti@unoesc.edu.br>
2025-02-21 15:28:55 -0300

    Adiciona o formatador de código prettier

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -5,61 +5,52 @@ import Button from '../Button';
 import { useState } from 'react';
 
 const Formulario = () => {
+  const times = ['', 'Educare', 'Protheus', 'BI', 'Head', 'Designer UX/UI'];
 
-    const times = [
-        '',
-        'Educare',
-        'Protheus',
-        'BI',
-        'Head',
-        'Designer UX/UI'
-    ]
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [time, setTime] = useState('');
 
-    const[nome,setNome] = useState('')
-    const[cargo,setCargo] = useState('')
-    const[imagem,setImagem] = useState('')
-    const[time, setTime] = useState('')
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log('Form submetido =>', nome, cargo, imagem, time);
+  };
 
-
-    const aoSalvar = (evento) =>{
-        evento.preventDefault()
-        console.log('Form submetido =>', nome, cargo, imagem, time)
-    }
-
-    return (
-        <section className='formulario'>
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto
-                   obrigatorio = {true}
-                   label="Nome"
-                   placeholder="Digite seu nome"
-                   valor={nome}
-                   aoAlterado={valor => setNome(valor)}
-                    />
-                <CampoTexto
-                  obrigatorio = {true}
-                  label="Cargo"
-                  placeholder="Digite seu cargo" 
-                  valor={cargo}
-                  aoAlterado={valor => setCargo(valor)}
-                   />
-                <CampoTexto
-                  label="Imagem"
-                  placeholder="Digite o endereço da imagem"
-                  valor={imagem}
-                  aoAlterado={valor => setImagem(valor)}
-                   />
-                <ListaSuspensa
-                 label= "Time"
-                 itens={times}
-                 valor={time}                 
-                 aoAlterado={valor => setTime(valor)}
-                 />
-                <Button>Criar Card</Button> 
-            </form>
-        </section>
-    )
-}
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          label="Time"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+        <Button>Criar Card</Button>
+      </form>
+    </section>
+  );
+};
 
 export default Formulario;
